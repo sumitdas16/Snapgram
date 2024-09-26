aed4b61ef9065d2982c627e2b808bf0498ae5854
sumitdas16 <sumitdasnaity@gmail.com>
2024-09-26 09:53:32 +0530

    post details page created

--- src/root/pages/PostDetails.tsx
+++ src/root/pages/PostDetails.tsx
@@ -1,11 +1,115 @@
-import React from 'react'
+import Loader from "@/components/shared/Loader";
+import PostStats from "@/components/shared/PostStats";
+import { Button } from "@/components/ui/button";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetPostById } from "@/lib/react-query/QueriesAndMutations";
+import { timeAgo } from "@/lib/utils";
+import { Link, useParams } from "react-router-dom";
 
 const PostDetails = () => {
-  return (
-    <div>
-      Post Details
-    </div>
-  )
-}
+  const { id } = useParams();
+  const { data: post, isPending } = useGetPostById(id || "");
+  const handleDeletePost = () => {};
 
-export default PostDetails
+  const { user } = useUserContext();
+  return (
+    <div className="post_details-container">
+      {isPending ? (
+        <Loader />
+      ) : (
+        <div className="post_details-card ">
+          <img
+            src={post?.imageUrl}
+            alt="post"
+            className="post-details-img p-3 lg:w-80 rounded-xl"
+          />
+
+          <div className="post-details-info pl-4 pt-5 pb-5 w-full">
+            <div className="flex-between w-full">
+              <Link
+                to={`/profile/${post?.creator.$id}`}
+                className="flex items-center gap-3"
+              >
+                <img
+                  src={
+                    post?.creator.imageUrl ||
+                    "/assets/icons/profile-placeholder.svg"
+                  }
+                  alt="creator"
+                  className="rounded-full w-8 h-8 lg:w-12 lg:h-12"
+                />
+
+                <div className="flex flex-col">
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {post?.creator.name}
+                  </p>
+                  <div className="flex-center gap-2 text-light-3">
+                    <p className="subtle-semibold lg:small-regular">
+                      {timeAgo(post?.$createdAt || "")}
+                    </p>
+                    -
+                    <p className="subtle-semibold lg:small-regular">
+                      {post?.location}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+
+              <div className="flex-center gap-4 ml-10 ">
+                <Link
+                  to={`/update-post/${post?.$id}`}
+                  className={`${user.id !== post?.creator.$id && "hidden"}`}
+                >
+                  <img
+                    src="/assets/icons/edit.svg"
+                    height={24}
+                    width={24}
+                    alt="edit"
+                  />
+                </Link>
+
+                <Button
+                  onClick={handleDeletePost}
+                  variant="ghost"
+                  className={`ghost_details_btn ${
+                    user.id !== post?.creator.$id && "hidden"
+                  }`}
+                >
+                  <img
+                    src="/assets/icons/delete.svg"
+                    alt="delete"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
+              </div>
+            </div>
+
+            <div className="mt-5">
+              <hr className="border w-full border-dark-4/80" />
+            </div>
+            <div className="flex flex-1 flex-col w-full small-medium lg:base-regular mt-3">
+              <p>{post?.caption}</p>
+              <ul className="flex gap-1 mt-2">
+                {post?.tags.map((tag: string, index: string) => (
+                  <li
+                    key={`${tag}${index}`}
+                    className="text-light-3 small-regular"
+                  >
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            
+            <div className="w-full pr-4 lg:pt-40">
+              <PostStats post={post} userId={user.id}/>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PostDetails;
